2bc25f076f580e98b2ec3309ab35fa4fda5b7fee
Natallia Zhamaitsiak <61240842+natallia-zzz@users.noreply.github.com>
2022-10-27 10:34:05 +0300

    added function for data extraction. added function for area calcutaltion using formulas from 2nd course paper

--- main.py
+++ main.py
@@ -3,14 +3,25 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+import meshpy as mp
+import json
+import sys
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def file_analysis(path):
+    f = open(path)
+    data = json.load(f)
+    return data['shapes']
+
+
+def cell_area(pts):
+    sys.path.append('/Users/natalliazzz/Downloads/BSU MMF/2coursepaper')
+    import volume
+    print(volume.area_of_triangle(pts[0], pts[1], pts[2]))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    file_analysis("Segmented 2/3.json")
+    cell_area([[1,2], [3,3],[2,1]])
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
